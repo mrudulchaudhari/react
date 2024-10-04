a98e74071a664db029ef1a1755d9cce9be5fa96d
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2024-10-04 16:02:53 +0530

    updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './card'
-import Button from './button/Button.jsx'
+import Button from './button/button'
 
 function App() {
   return(
