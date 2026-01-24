643015c70bb6cf9dfb5fba61807f90b7ee277696
Mrudul <mrudulchaudhari0608@gmail.com>
2026-01-24 22:33:00 +0530

    functions in js

---
+++ js-revision/basic.js
@@ -0,0 +1,11 @@
+console.log("Hello world")
+console.log("5" == 5)
+console.log("5" === 5)
+
+function changeFont(id){
+    document.getElementById(id).style.fontSize = "35px"; 
+}
+
+function changeContent(id){
+    document.getElementById(id).innerHTML = "Content changed";
+}
