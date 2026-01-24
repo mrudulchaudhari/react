534c16c2d370d2146785ca30097c800d77d3a54c
Mrudul <mrudulchaudhari0608@gmail.com>
2026-01-24 23:19:36 +0530

    basics of js object

--- js-revision/basic.js
+++ js-revision/basic.js
@@ -78,3 +78,47 @@ const addition = (a, b) => {
 };
 
 console.log(addition(4, 3));
+
+// JS Objects
+const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+};
+console.log(car);
+console.log(car.type);
+console.log(car.model);
+console.log(car.color);
+
+const person = {};
+
+// Add Properties
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+const person2 = new Object({
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+});
+
+console.log(person);
+console.log(person2);
+console.log(person["firstName"])
+
+delete person.age;
+console.log(person);
+
+const person3 = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+console.log(person3.fullName());
