26663a5228ec2d6ea3b741b94a1a088877a2d9d2
Mrudul <mrudulchaudhari0608@gmail.com>
2026-01-24 22:37:56 +0530

    made it a loop struct

--- js-revision/basic.js
+++ js-revision/basic.js
@@ -2,10 +2,27 @@ console.log("Hello world")
 console.log("5" == 5)
 console.log("5" === 5)
 
+
+changed = false;
 function changeFont(id){
-    document.getElementById(id).style.fontSize = "35px"; 
+    if (!changed){
+        document.getElementById(id).style.fontSize = "35px";
+         
+    }    
+    else {
+        document.getElementById(id).style.fontSize = "15px"; 
+    }
+    changed = !changed;
 }
 
+changedC = false;
 function changeContent(id){
-    document.getElementById(id).innerHTML = "Content changed";
+    if (!changedC){
+        document.getElementById(id).innerHTML = "Content changed";
+    }
+    else{
+        document.getElementById(id).innerHTML = "Original";
+    }
+    changedC = !changedC;
+    
 }
